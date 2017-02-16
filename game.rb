93de6f18fd1df1bfb6639eb4cb9749af930a0937
Laura <Laura.Stephens@utexas.edu>
2017-02-15 18:15:35 -0600

    achieve basic functionality

--- game.rb
+++ game.rb
@@ -1,30 +1,35 @@
-require 'sinatra'
-#require 'sinatra/reloader'
-require './mastermind'
-background = "background: yellow"
-p = PlayerGame.new
+ require 'sinatra'
+ require 'sinatra/reloader' if development? 
+ require './mastermind'
 
-get '/' do 
+ p = PlayerGame.new
+ get '/' do 
 
-	if !params['guess'].nil? && valid_guess(params['guess'])
-		guess = params['guess'].downcase.chomp.split(" ")
-		game_board = p.game_board(guess)
-		game_hints = p.check_guess(guess)
-		guesses_left = p.max_guess_count - p.guess_count
-		if p.game_over?
-			game_over_message = p.game_over_message
-		end
-	end
-	
-
-	erb :index, :locals => { :game_board => game_board, :game_hints => game_hints 
-		:background => background, :guesses_left => guesses_left,
-		:game_over_message => game_over_message}
-	end
-
+ 	code = p.code
+ 	puts code
+ 	guesses_left = p.max_guess_count - p.guess_count
+ 	
+ 	if !params['guess'].nil? 
+ 		guess = params['guess'].downcase.chomp.split(" ")
+ 		if p.valid_guess(guess)
+ 			game_board = p.game_board(guess)
+ 			game_hints = p.check_guess(guess)
+ 			if p.game_over?(guess)
+ 				message = p.game_over_message(guess)
+ 			end
+ 		end
+ 		 error_message = p.error_message
+ 	end
 
 
 
+ 	erb :index, :locals => { :game_board => game_board, :game_hints => game_hints, 
+ 		:guesses_left => guesses_left, :error_message => error_message,
+ 		:message => message }
 
+ end
 
+ # p = PlayerGame.new
+ # p.valid_guess("blue".downcase.chomp.split(" "))
+ # puts p.error_message
 
